0edd28cad30e457b396549eb1d6f5fd1ea7e9a7a
Lucas Xavier <lucasxavier523@gmail.com>
2024-10-10 18:24:20 -0300

    Criando o Layout menu principal

--- Fin.Web/Configuration.cs
+++ Fin.Web/Configuration.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace Fin.Web
 {
@@ -9,5 +11,25 @@ namespace Fin.Web
     {
         public const string HttpClientName = "Api";
         public static string BackendUrl { get; set; } = "http://localhost:5110";
+
+        public static MudTheme Theme = new()
+        {
+            Typography = new Typography
+            {
+                Default = new Default
+                {
+                    FontFamily = ["Raleway", "sans-serif"]
+                }
+            },
+            PaletteDark = new PaletteDark
+            {
+                Primary = Colors.LightGreen.Accent3,
+                Secondary = Colors.LightGreen.Darken3,
+                // Background = Colors.LightGreen.Darken4,
+                AppbarBackground = Colors.LightGreen.Accent3,
+                AppbarText = Colors.Shades.Black,
+                PrimaryContrastText = new MudColor("#000000")
+            }
+        };
     }
 }
