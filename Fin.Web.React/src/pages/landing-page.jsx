5663b9df3f5649f7e51439892a8cff7e9b469877
LucasXavier <lucasxavier523@gmail.com>
2025-03-29 11:21:38 -0300

    Ajustes nas letras do landing page e formatações de botões

--- Fin.Web.React/src/pages/landing-page.jsx
+++ Fin.Web.React/src/pages/landing-page.jsx
@@ -5,12 +5,12 @@ const LandingPage = () => {
 
   return (
     <div className="text-center p-10">
-      <h1 className="text-4xl font-bold mb-4">Bem-vindo ao Financial Management</h1>
-      <p className="text-lg mb-6">Gerencie suas finanças com facilidade e segurança.</p>
-      
-      <button 
+      <h1 className="text-4xl font-semibold mb-4 text-gray-800 dark:text-gray-200">Bem-vindo ao Minhas Finanças</h1>
+      <p className="text-lg font-semibold mb-6 text-gray-800 dark:text-gray-200">Gerencie suas finanças com facilidade e segurança.</p>
+            
+      <button
         onClick={() => navigate("/dashboard")}
-        className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition"
+        className="px-6 py-3 sm:flex-grow-0 bg-green-600 dark:bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-green-700 dark:hover:bg-blue-600 transition-colors"
       >
         Acessar Demonstração
       </button>
